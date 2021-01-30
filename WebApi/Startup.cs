5f3a2ee6dc6fa4331c41c16281300da33c1401cf
Kevin Xiong <bearorpanda@163.com>
2021-01-30 22:47:02 +0800

    Format code: use 2 spaces instead of 4 spaces

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -15,82 +15,82 @@ using Swashbuckle.AspNetCore.Swagger;
 
 namespace WebApi
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
-
-            // Register the Swagger generator, defining 1 or more Swagger documents
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Title = "Awesome Web API",
-                    Version = "1.0.0",
-                    Description = "A simple example of ASP.NET Core Web API, You can find out more about Swagger at http://swagger.io.",
-                    TermsOfService = new Uri("https://smartbear.com/terms-of-use/"),
-                    Contact = new OpenApiContact
-                    {
-                        Name = "Kevin Xiong",
-                        Email = string.Empty,
-                        Url = new Uri("https://github.com/ToMakeSense"),
-                    },
-                    License = new OpenApiLicense
-                    {
-                        Name = "GNU GPL 3.0",
-                        Url = new Uri("https://www.gnu.org/licenses/gpl-3.0.en.html"),
-                    }
-                });
-            });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            // Enable middleware to serve generated Swagger as a JSON endpoint.
-            app.UseSwagger();
-
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
-            // specifying the Swagger JSON endpoint.
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Awesome Web API V1");
-
-                // Serve the Swagger UI at the app's root
-                c.RoutePrefix = string.Empty;
-
-                c.InjectStylesheet("/swagger/ui/custom.css");
-            });
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddControllers();
+
+      // Register the Swagger generator, defining 1 or more Swagger documents
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+          Title = "Awesome Web API",
+          Version = "1.0.0",
+          Description = "A simple example of ASP.NET Core Web API, You can find out more about Swagger at http://swagger.io.",
+          TermsOfService = new Uri("https://smartbear.com/terms-of-use/"),
+          Contact = new OpenApiContact
+          {
+            Name = "Kevin Xiong",
+            Email = string.Empty,
+            Url = new Uri("https://github.com/ToMakeSense"),
+          },
+          License = new OpenApiLicense
+          {
+            Name = "GNU GPL 3.0",
+            Url = new Uri("https://www.gnu.org/licenses/gpl-3.0.en.html"),
+          }
+        });
+      });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+      }
+
+      // Enable middleware to serve generated Swagger as a JSON endpoint.
+      app.UseSwagger();
+
+      // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+      // specifying the Swagger JSON endpoint.
+      app.UseSwaggerUI(c =>
+      {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Awesome Web API V1");
+
+        // Serve the Swagger UI at the app's root
+        c.RoutePrefix = string.Empty;
+
+        c.InjectStylesheet("/swagger/ui/custom.css");
+      });
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
