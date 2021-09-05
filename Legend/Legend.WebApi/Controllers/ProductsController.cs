5dd8c34120950f8bfa98833769cc861015fc01b6
Kevin Xiong <bearorpanda@163.com>
2021-09-05 18:59:00 +0800

    Add sample model + controller, demonstration the simple usage of query APIs
    Get the entire products --> http://localhost:60008/api/Products
    Look up a single product--> http://localhost:60008/api/Products/1

---
+++ Legend/Legend.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Legend.WebApi.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Web.Http;
+
+namespace WebApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 2 },
+            new Product { Id = 2, Name = "Teddy Bear", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Laptop", Category = "Hardware", Price = 999.9M }
+        };
+
+        // GET api/Products
+        public IEnumerable<Product> Get()
+        {
+            return products;
+        }
+
+        // GET api/Products/1
+        public IHttpActionResult Get(int id)
+        {
+            var product = products.Where(p => p.Id == id).FirstOrDefault();
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
